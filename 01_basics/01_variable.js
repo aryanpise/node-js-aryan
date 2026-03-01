8d785f019c4bf9dc7e6db9b38bd8ff577c355d7e
aryanpise <aryanpise6@gmail.com>
2026-03-01 20:54:21 +0000

    data type

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountID = 9049
+let accountgmail = "aryan@2gmail.com"
+var accountpassward = "1234"
+accountCity = "waghnakh" 
+let accountdate;
+
+accountgmail ="aryan@3gmail.com"
+accountpassward = "11111"
+accountCity = "mumbai"
+
+console.log("accountID");
+console.table([accountgmail, accountpassward, accountCity, accountdate]);
+
---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,19 @@
+"use strict" /* treat all js as newest verson*/
+// alert (3 + 3) we are using nodejs not browser//
+
+let name = "aryan"
+let age = 17
+let islogin = "false/true"
+
+// number=> 2 to thr power of53
+// bigint
+// string=>""
+// boolean=> true/false
+// null=> sandalone value
+// undefined=>
+// symbol=>define uniquness
+
+// object
+console.log(typeof "age");
+
+
