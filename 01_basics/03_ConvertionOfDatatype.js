1f1f72336fe11651ab4e7166c46820e1c8fd1e14
aryanpise <aryanpise6@gmail.com>
2026-03-23 18:29:49 +0000

    convertion of data type

---
+++ 01_basics/03_ConvertionOfDatatype.js
@@ -0,0 +1,28 @@
+let score= true
+// console.log(typeof (score));
+
+let valueinnumber= Number(score);
+// console.log(typeof valueinnumber);
+// console.log(valueinnumber);
+
+// "33"=> easily convert 33
+// "33abc"=>NaN(NOTE A NUMBER)
+// false=>0
+// true=>1
+
+let islogedin = 1
+let booleanislogedin = Boolean(islogedin);
+// console.log(booleanislogedin)
+
+// 1=>true
+// 0=>false
+// ""=>false
+// "aryan"=>true
+// null=>false
+// undefined=>false
+
+let somenumber = 88
+
+let stringsomenumber = String(somenumber)
+console.log(stringsomenumber)
+console.log( typeof stringsomenumber)
